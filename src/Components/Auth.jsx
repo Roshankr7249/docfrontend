d7c4ebc793ce58c3a85f58a70df3ac9ecc5204a3
Roshan Kumar <125895776+Roshankr7249@users.noreply.github.com>
2024-07-10 13:49:21 +0530

    Update Auth.jsx

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
+<script src="https://assets.adobedtm.com/21b53c73144b/bd73161192e0/launch-6dd8d4d01c07-development.min.js" async></script>
 
 const App = () => {
   const [login, setLogin] = useState(true);
